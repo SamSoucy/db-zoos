58f6115404c4535b7fd5c61241f26472f8d2ab06
Samsoucy <newf930@outlook.com>
2019-02-26 18:05:29 -0500

    added put to update a bear in the list

--- bears/bears-router.js
+++ bears/bears-router.js
@@ -52,7 +52,30 @@ router.post('/', (req, res) => {
     .catch(err => {
       res.status(500).json({message: "could not add new bear"});
     })
-  });
+});
+  
+/****************Update a bear in the list***************/
+
+router.put('/:id', (req, res) => {
+    db('bears')
+     .where({ id: req.params.id })
+     .update(req.body)
+     .then(response => {
+       if(response > 0) {
+         db('bears')
+         .where({ id: req.params.id })
+         .first()
+         .then(response => {
+           res.status(200).json(response)
+         })
+       } else {
+         res.status(404).json({ message: 'bear not found' })
+       }
+     })
+     .catch(error => {
+       res.status(500).json(error)
+     })
+   });
 
 
 
